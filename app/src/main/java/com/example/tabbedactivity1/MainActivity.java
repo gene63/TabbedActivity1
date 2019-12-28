ac331b62575fef3fe4cc79f88935ab8cbd7046de
KwonHyungGeun <gene63@naver.com>
2019-12-29 02:18:57 +0900

    Finish Tab1

--- app/src/main/java/com/example/tabbedactivity1/MainActivity.java
+++ app/src/main/java/com/example/tabbedactivity1/MainActivity.java
@@ -1,12 +1,5 @@
 package com.example.tabbedactivity1;
 
-
-import java.io.InputStream;
-import java.io.IOException;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,10 +8,8 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+
 
 import com.example.tabbedactivity1.ui.main.SectionsPagerAdapter;
 
@@ -43,71 +34,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    /*
-    private String getJsonString()
-    {
-        String json = "";
 
-        try {
-            InputStream is = getAssets().open("phonenum.json");
-            int fileSize = is.available();
-
-            byte[] buffer = new byte[fileSize];
-            is.read(buffer);
-            is.close();
-
-            json = new String(buffer, "UTF-8");
-        }
-        catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-
-        return json;
-    }
-
-    public class PhoneNum{
-        private String name;
-        private String number;
-
-        public String getName() {
-            return name;
-        }
-
-        public String getNumber() {
-            return number;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setNumber(String number) {
-            this.number = number;
-        }
-    }
-    private void jsonParsing(String json)
-    {
-        try{
-            JSONObject jsonObject = new JSONObject(json);
-
-            JSONArray phonenumArray = jsonObject.getJSONArray("phonenum");
-
-            for(int i=0; i<phonenumArray.length(); i++)
-            {
-                JSONObject phonenumObject = phonenumArray.getJSONObject(i);
-
-                PhoneNum pn = new PhoneNum();
-
-                pn.setName(phonenumObject.getString("name"));
-                pn.setNumber(phonenumObject.getString("number"));
-
-                //phonenumArray.add(pn);
-            }
-        }catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-    }*/
 }
--- app/src/main/java/com/example/tabbedactivity1/ui/main/phonenumberFragment.java
+++ app/src/main/java/com/example/tabbedactivity1/ui/main/phonenumberFragment.java
@@ -1,26 +1,41 @@
 package com.example.tabbedactivity1.ui.main;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.tabbedactivity1.R;
 
-/**
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.DriverManager.println;
+
+ /**
  * A placeholder fragment containing a simple view.
  */
-public class phonenumberFragment extends Fragment {
+public class phonenumberFragment extends ListFragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
     private PageViewModel pageViewModel;
 
     public static phonenumberFragment newInstance(int index) {
@@ -34,27 +49,74 @@ public class phonenumberFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
         int index = 1;
         if (getArguments() != null) {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+
+         */
     }
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_phonenumber, container, false);
-        /*final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-               textView.setText(s);
+
+        //새로운 코드
+        jsonParsing(getJsonString());
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    //json 관련 코드
+
+    //json 파일 열어서 string으로 가져오는 함수
+    private String getJsonString()
+    {
+        String json = "";
+
+        try {
+            AssetManager am = getResources().getAssets();
+            InputStream is = am.open("phonenum.json");
+            int fileSize = is.available();
+
+            byte[] buffer = new byte[fileSize];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+
+
+
+    private void jsonParsing(String json)
+    {
+        ListViewAdapter adapter = new ListViewAdapter() ;
+        setListAdapter(adapter);
+
+        try{
+            JSONArray phonenumArray = new JSONArray(json);
+
+            Log.d(String.valueOf(phonenumArray.length()),"2");
+
+            for(int i=0; i<phonenumArray.length(); i++)
+            {
+
+                JSONObject phonenumObject = phonenumArray.getJSONObject(i);
+                String phone_number = phonenumObject.getString("phone_number");
+                String name = phonenumObject.getString("name");
+                adapter.addItem(name, phone_number);
             }
-        });
-         */
-        return root;
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
